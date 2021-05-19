ab3d5f09e65310aa4c89b0556a73483a109649ad
Luke Chatelain <jens.chatelain@gmail.com>
2021-05-19 21:19:47 +0900

    Easy airbnbc completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "...clearing database"
+
+Flat.destroy_all
+
+puts "...Creating flats"
+
+20.times do
+  Flat.create(
+    name: Faker::Fantasy::Tolkien.location,
+    address: Faker::Address.street_address,
+    description: Faker::Fantasy::Tolkien.poem,
+    price_per_night: rand(100..200),
+    number_of_guests: rand(1..10)
+    )
+end
+puts "... created #{Flat.count} flats"
+
